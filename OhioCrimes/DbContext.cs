07342811a2263be666e6e903c312ec8bd4b541c8
Matthew Barnewall <mbarnewall@matrixpointesoftware.com>
2020-02-17 09:28:13 -0500

    adds sqlite database to website

---
+++ OhioCrimes/DbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OhioCrimes
+{
+  public class DbContext :  Microsoft.EntityFrameworkCore.DbContext
+  {
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlite("Filename=./ohiocrimes.db");
+    }
+  }
+}
